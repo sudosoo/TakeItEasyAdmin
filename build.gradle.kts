b191792569cd8b21a07e4e5c5bf8a3bb833c13be
username <email@email.com>
2024-02-12 18:28:28 +0900

    git init

---
+++ build.gradle.kts
@@ -0,0 +1,44 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "3.1.0"
+    id("io.spring.dependency-management") version "1.1.0"
+    kotlin("jvm") version "1.8.21"
+    kotlin("plugin.spring") version "1.8.21"
+    kotlin("plugin.jpa") version "1.8.21"
+    application
+}
+
+group = "com.sudoSoo"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    //로그 의존성
+    implementation("io.github.microutils:kotlin-logging-jvm:2.0.11")
+
+    //Spring Data 의존성
+    implementation("org.springframework.data:spring-data-envers")
+    runtimeOnly ("com.h2database:h2")
+    implementation ("org.springframework.boot:spring-boot-starter-data-jpa")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
