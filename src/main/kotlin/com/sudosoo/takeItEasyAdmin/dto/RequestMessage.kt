226910984808b3bb9df6ff0e032d0e8218919c32
username <email@email.com>
2024-02-21 18:20:35 +0900

    fix/directory location

--- src/main/kotlin/com/sudosoo/takeItEasyAdmin/dto/RequestMessage.kt
+++
@@ -1,7 +0,0 @@
-package com.sudosoo.takeItEasyAdmin.com.sudosoo.takeItEasyAdmin.dto
-
-import com.sudosoo.takeItEasyAdmin.dto.GetMemberRequestDto
-
-class RequestMessage (val targetMethod:String ,val requestDto : GetMemberRequestDto){
-
-}
