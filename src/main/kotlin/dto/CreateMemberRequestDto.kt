0edee6bce977801374e708cc19ccf09107c63b1b
username <email@email.com>
2024-02-21 18:07:08 +0900

    fix/dto field

--- src/main/kotlin/dto/CreateMemberRequestDto.kt
+++ src/main/kotlin/dto/CreateMemberRequestDto.kt
@@ -1,4 +1,3 @@
 package com.sudosoo.takeItEasyAdmin.dto
 
-class CreateMemberRequestDto(var memberName: String = "사용자명을 입력해 주세요."
-)
+class CreateMemberRequestDto(var memberName: String = "사용자명을 입력해 주세요.")
