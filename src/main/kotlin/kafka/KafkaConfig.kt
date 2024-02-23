457d1c44e4b8a73d0b263f546a5cc7f611b31efb
username <email@email.com>
2024-02-24 00:27:03 +0900

    feat/kafka replyKafkaTemplate 추가

--- src/main/kotlin/kafka/KafkaConfig.kt
+++ src/main/kotlin/kafka/KafkaConfig.kt
@@ -5,18 +5,28 @@ import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.config.KafkaListenerContainerFactory
 import org.springframework.kafka.core.*
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer
+import org.springframework.kafka.listener.ContainerProperties
+import org.springframework.kafka.listener.KafkaMessageListenerContainer
+import org.springframework.kafka.requestreply.ReplyingKafkaTemplate
 import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.kafka.support.serializer.JsonSerializer
 
 @Configuration
-class KafkaConfig(
+class KafkaConfig {
+
     @Value("\${spring.kafka.bootstrap-servers}")
-    val bootstrapAddress: String
-){
+    private lateinit var bootstrapAddress: String
+
+    @Value("\${devsoo.kafka.restapi.topic}")
+    private lateinit var requestReplyTopic: String
+
     @Bean
     fun producerFactory(): ProducerFactory<String, Any> {
-        val props = mutableMapOf<String, Any>()
+        val props: MutableMap<String, Any> = HashMap()
         props[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapAddress
         props[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
         props[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
@@ -25,15 +35,35 @@ class KafkaConfig(
 
     @Bean
     fun consumerFactory(): ConsumerFactory<String, Any> {
-        val props = mutableMapOf<String, Any>()
+        val props: MutableMap<String, Any> = HashMap()
         props[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapAddress
         props[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
         props[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
         props[JsonDeserializer.TRUSTED_PACKAGES] = "*"
         return DefaultKafkaConsumerFactory(props)
     }
+
     @Bean
     fun kafkaTemplate(): KafkaTemplate<String, Any> {
         return KafkaTemplate(producerFactory())
+    }
+
+    @Bean
+    fun replyKafkaTemplate(pf: ProducerFactory<String, Any>, container: KafkaMessageListenerContainer<String, Any>): ReplyingKafkaTemplate<String, Any, Any> {
+        return ReplyingKafkaTemplate(pf, container)
+    }
+
+    @Bean
+    fun replyContainer(cf: ConsumerFactory<String, Any>): KafkaMessageListenerContainer<String, Any> {
+        val containerProperties = ContainerProperties(requestReplyTopic)
+        return KafkaMessageListenerContainer(cf, containerProperties)
+    }
+
+    @Bean
+    fun kafkaListenerContainerFactory(): KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, Any>> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
+        factory.consumerFactory = consumerFactory()
+        factory.setReplyTemplate(kafkaTemplate())
+        return factory
     }
 }
